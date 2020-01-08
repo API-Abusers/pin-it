62c4b8e591cb178796642d5ae5a74c199b3773dd
AnimatorJoe <josephjin7@gmail.com>
2020-01-08 12:27:26 -0600

    initializing cocoapods and adding license

---
+++ Pods/Target Support Files/Pods-Pin It-Pin ItUITests/Pods-Pin It-Pin ItUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pin_It_Pin_ItUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Pin_It_Pin_ItUITests
+@end
---
+++ Pods/Target Support Files/Pods-Pin ItTests/Pods-Pin ItTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pin_ItTests : NSObject
+@end
+@implementation PodsDummy_Pods_Pin_ItTests
+@end
