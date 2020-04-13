f8d0043b557a6b440f150b184c70f5d634835207
AnimatorJoe <josephjin7@gmail.com>
2020-04-12 21:42:32 -0500

    rendering exit and options button on detail view page

--- Pin It/Main View Controllers/DetailedPostLayout.swift
+++ Pin It/Main View Controllers/DetailedPostLayout.swift
@@ -10,14 +10,28 @@ import UIKit
 import LayoutKit
 import ImageSlideshow
 import PromiseKit
+import Firebase
 
 public class DetailedPostLayout: InsetLayout<UIView> {
 
     init(_ entry: Entry, rootvc: UIViewController) {
         
         let titleLayout = LabelLayout(text: entry.title, font: UIFont.boldSystemFont(ofSize: 40))
+        
+        let editButtonLayout = ButtonLayout(type: .system, title: "", contentEdgeInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0), alignment: .topTrailing, flexibility: Flexibility.inflexible) { button in
+            button.setBackgroundImage(UIImage(systemName: "ellipsis.circle.fill"), for: .normal)
+            button.tintColor = .white
+        }
+        
+        let exitButtonLayout = ButtonLayout(type: .system, title: "", contentEdgeInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0), alignment: .topTrailing, flexibility: Flexibility.inflexible) { button in
+            button.setBackgroundImage(UIImage(systemName: "xmark.circle.fill"), for: .normal)
+            button.tintColor = .white
+            button.addTapGestureRecognizer { rootvc.dismiss(animated: true) }
+        }
+        
         let authorLayout = LabelLayout(text: entry.username, font: UIFont.italicSystemFont(ofSize: 15))
         let descLayout = LabelLayout(text: entry.desc, font: UIFont.systemFont(ofSize: 20))
+        
         let imageSlideshow = SizeLayout<ImageSlideshow>(
             width: rootvc.view.frame.size.width - 50,
             height: rootvc.view.frame.size.width - 50,
@@ -52,18 +66,29 @@ public class DetailedPostLayout: InsetLayout<UIView> {
                     }
                 }.catch { (err) in
                     print("[DetailedPostLayout] Error while loading images: \(err)")
+                    spinner.stopAnimating()
                 }.finally {
                     spinner.stopAnimating()
                 }
             }
         )
+        
+        var topLayout: [Layout] = [titleLayout, exitButtonLayout]
+        let uid = Auth.auth().currentUser?.uid ?? "none"
+        if (entry.owner == uid) {
+            topLayout = [titleLayout, editButtonLayout, exitButtonLayout]
+        }
             
         super.init(
             insets: UIEdgeInsets(top: 25, left: 25, bottom: 0, right: 25),
             sublayout: StackLayout(
                 axis: .vertical,
                 spacing: 25,
-                sublayouts: [titleLayout, authorLayout, imageSlideshow, descLayout]
+                sublayouts: [
+                    StackLayout(axis: .horizontal, spacing: 5, sublayouts: topLayout),
+                    authorLayout,
+                    imageSlideshow,
+                    descLayout]
             )
         )
     
